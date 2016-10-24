c0267a48a6c6b56d40b7c864721435dac386915e
Ruchi Ookalkar <rookalkar@gmail.com>
2016-10-25 01:28:46 +0530

    Add files via upload

---
+++ Data2001.js
@@ -0,0 +1,492 @@
+var data2001 = {
+   "ANDAMAN AND NICOBAR": {
+      "Year": 2001,
+      "Total": 3
+   },
+   "ANDHRA PRADESH": {
+      "Year": 2001,
+      "Total": 1509
+   },
+   "ARUNACHAL PRADESH": {
+      "Year": 2001,
+      "Total": 17
+   },
+   "ASSAM": {
+      "Year": 2001,
+      "Total": 167
+   },
+   "BIHAR": {
+      "Year": 2001,
+      "Total": 61
+   },
+   "CHANDIGARH": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "CHHATTISGARH": {
+      "Year": 2001,
+      "Total": 1452
+   },
+   "DADRA AND NAGAR HAVELI": {
+      "Year": 2001,
+      "Total": 19
+   },
+   "DAMAN AND DIU": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "DELHI": {
+      "Year": 2001,
+      "Total": 18
+   },
+   "GOA": {
+      "Year": 2001,
+      "Total": 18
+   },
+   "GUJARAT": {
+      "Year": 2001,
+      "Total": 594
+   },
+   "HARYANA": {
+      "Year": 2001,
+      "Total": 145
+   },
+   "HIMACHAL PRADESH": {
+      "Year": 2001,
+      "Total": 22
+   },
+   "JAMMU AND KASHMIR": {
+      "Year": 2001,
+      "Total": 15
+   },
+   "JHARKHAND": {
+      "Year": 2001,
+      "Total": 27
+   },
+   "KARNATAKA": {
+      "Year": 2001,
+      "Total": 2505
+   },
+   "KERALA": {
+      "Year": 2001,
+      "Total": 1035
+   },
+   "LAKSHADWEEP": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "MADHYA PRADESH": {
+      "Year": 2001,
+      "Total": 1372
+   },
+   "MAHARASHTRA": {
+      "Year": 2001,
+      "Total": 3536
+   },
+   "MANIPUR": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "MEGHALAYA": {
+      "Year": 2001,
+      "Total": 4
+   },
+   "MIZORAM": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "NAGALAND": {
+      "Year": 2001,
+      "Total": 0
+   },
+   "ORISSA": {
+      "Year": 2001,
+      "Total": 256
+   },
+   "PUDUCHERRY": {
+      "Year": 2001,
+      "Total": 91
+   },
+   "PUNJAB": {
+      "Year": 2001,
+      "Total": 45
+   },
+   "RAJASTHAN": {
+      "Year": 2001,
+      "Total": 505
+   },
+   "SIKKIM": {
+      "Year": 2001,
+      "Total": 18
+   },
+   "TAMIL NADU": {
+      "Year": 2001,
+      "Total": 985
+   },
+   "TOTAL (ALL INDIA)": {
+      "Year": 2001,
+      "Total": 16415
+   },
+   "TOTAL (STATES)": {
+      "Year": 2001,
+      "Total": 16284
+   },
+   "TOTAL (UTs)": {
+      "Year": 2001,
+      "Total": 131
+   },
+   "TRIPURA": {
+      "Year": 2001,
+      "Total": 41
+   },
+   "UTTAR PRADESH": {
+      "Year": 2001,
+      "Total": 688
+   },
+   "UTTARAKHAND": {
+      "Year": 2001,
+      "Total": 21
+   },
+   "UTTARANCHAL": {
+      "Year": 2001,
+      "Total": 21
+   },
+   "WEST BENGAL": {
+      "Year": 2001,
+      "Total": 1246
+   }
+}
+
+var data2002 = {
+   "ANDAMAN AND NICOBAR": {
+      "Year": 2002,
+      "Total": 7
+   },
+   "ANDHRA PRADESH": {
+      "Year": 2002,
+      "Total": 1896
+   },
+   "ARUNACHAL PRADESH": {
+      "Year": 2002,
+      "Total": 27
+   },
+   "ASSAM": {
+      "Year": 2002,
+      "Total": 271
+   },
+   "BIHAR": {
+      "Year": 2002,
+      "Total": 80
+   },
+   "CHANDIGARH": {
+      "Year": 2002,
+      "Total": 0
+   },
+   "CHHATTISGARH": {
+      "Year": 2002,
+      "Total": 1238
+   },
+   "DADRA AND NAGAR HAVELI": {
+      "Year": 2002,
+      "Total": 21
+   },
+   "DAMAN AND DIU": {
+      "Year": 2002,
+      "Total": 2
+   },
+   "DELHI": {
+      "Year": 2002,
+      "Total": 33
+   },
+   "GOA": {
+      "Year": 2002,
+      "Total": 11
+   },
+   "GUJARAT": {
+      "Year": 2002,
+      "Total": 570
+   },
+   "HARYANA": {
+      "Year": 2002,
+      "Total": 190
+   },
+   "HIMACHAL PRADESH": {
+      "Year": 2002,
+      "Total": 25
+   },
+   "JAMMU AND KASHMIR": {
+      "Year": 2002,
+      "Total": 16
+   },
+   "JHARKHAND": {
+      "Year": 2002,
+      "Total": 21
+   },
+   "KARNATAKA": {
+      "Year": 2002,
+      "Total": 2340
+   },
+   "KERALA": {
+      "Year": 2002,
+      "Total": 1533
+   },
+   "LAKSHADWEEP": {
+      "Year": 2002,
+      "Total": 0
+   },
+   "MADHYA PRADESH": {
+      "Year": 2002,
+      "Total": 1340
+   },
+   "MAHARASHTRA": {
+      "Year": 2002,
+      "Total": 3695
+   },
+   "MANIPUR": {
+      "Year": 2002,
+      "Total": 3
+   },
+   "MEGHALAYA": {
+      "Year": 2002,
+      "Total": 8
+   },
+   "MIZORAM": {
+      "Year": 2002,
+      "Total": 3
+   },
+   "NAGALAND": {
+      "Year": 2002,
+      "Total": 0
+   },
+   "ORISSA": {
+      "Year": 2002,
+      "Total": 345
+   },
+   "PUDUCHERRY": {
+      "Year": 2002,
+      "Total": 88
+   },
+   "PUNJAB": {
+      "Year": 2002,
+      "Total": 40
+   },
+   "RAJASTHAN": {
+      "Year": 2002,
+      "Total": 587
+   },
+   "SIKKIM": {
+      "Year": 2002,
+      "Total": 8
+   },
+   "TAMIL NADU": {
+      "Year": 2002,
+      "Total": 1455
+   },
+   "TOTAL (ALL INDIA)": {
+      "Year": 2002,
+      "Total": 17971
+   },
+   "TOTAL (STATES)": {
+      "Year": 2002,
+      "Total": 17820
+   },
+   "TOTAL (UTs)": {
+      "Year": 2002,
+      "Total": 151
+   },
+   "TRIPURA": {
+      "Year": 2002,
+      "Total": 41
+   },
+   "UTTAR PRADESH": {
+      "Year": 2002,
+      "Total": 525
+   },
+   "UTTARAKHAND": {
+      "Year": 2002,
+      "Total": 34
+   },
+    
+   "UTTARANCHAL": {
+      "Year": 2002,
+      "Total": 34
+   },
+    
+   "WEST BENGAL": {
+      "Year": 2002,
+      "Total": 1518
+   }
+}
+
+var data2014 = 
+    {
+   "ANDAMAN AND NICOBAR": {
+      "Year": 2014,
+      "Total": 8
+   },
+   "ANDHRA PRADESH": {
+      "Year": 2014,
+      "Total": 632
+   },
+   "ARUNACHAL PRADESH": {
+      "Year": 2014,
+      "Total": 3
+   },
+   "ASSAM": {
+      "Year": 2014,
+      "Total": 59
+   },
+   "BIHAR": {
+      "Year": 2014,
+      "Total": 10
+   },
+   "CHANDIGARH": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "CHHATTISGARH": {
+      "Year": 2014,
+      "Total": 755
+   },
+   "DADRA AND NAGAR HAVELI": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "DAMAN AND DIU": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "DELHI": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "GOA": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "GUJARAT": {
+      "Year": 2014,
+      "Total": 600
+   },
+   "HARYANA": {
+      "Year": 2014,
+      "Total": 119
+   },
+   "HIMACHAL PRADESH": {
+      "Year": 2014,
+      "Total": 63
+   },
+   "JAMMU AND KASHMIR": {
+      "Year": 2014,
+      "Total": 37
+   },
+   "JHARKHAND": {
+      "Year": 2014,
+      "Total": 4
+   },
+   "KARNATAKA": {
+      "Year": 2014,
+      "Total": 768
+   },
+   "KERALA": {
+      "Year": 2014,
+      "Total": 807
+   },
+   "LAKSHADWEEP": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "MADHYA PRADESH": {
+      "Year": 2014,
+      "Total": 1198
+   },
+   "MAHARASHTRA": {
+      "Year": 2014,
+      "Total": 4004
+   },
+   "MANIPUR": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "MEGHALAYA": {
+      "Year": 2014,
+      "Total": 2
+   },
+   "MIZORAM": {
+      "Year": 2014,
+      "Total": 5
+   },
+   "NAGALAND": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "ORISSA": {
+      "Year": 2014,
+      "Total": 102
+   },
+   "PUDUCHERRY": {
+      "Year": 2014,
+      "Total": 16
+   },
+   "PUNJAB": {
+      "Year": 2014,
+      "Total": 64
+   },
+   "RAJASTHAN": {
+      "Year": 2014,
+      "Total": 373
+   },
+   "SIKKIM": {
+      "Year": 2014,
+      "Total": 35
+   },
+   "TAMIL NADU": {
+      "Year": 2014,
+      "Total": 895
+   },
+   "TELANGANA": {
+      "Year": 2014,
+      "Total": 1347
+   },
+   "Total (All India)": {
+      "Year": 2014,
+      "Total": 12360
+   },
+   "Total (States)": {
+      "Year": 2014,
+      "Total": 12336
+   },
+   "Total (UTs)": {
+      "Year": 2014,
+      "Total": 24
+   },
+   "TRIPURA": {
+      "Year": 2014,
+      "Total": 32
+   },
+   "UTTAR PRADESH": {
+      "Year": 2014,
+      "Total": 192
+   },
+   "UTTARAKHAND": {
+      "Year": 2014,
+      "Total": 0
+   },
+        
+   "UTTARANCHAL": {
+      "Year": 2014,
+      "Total": 0
+   },
+   "WEST BENGAL": {
+      "Year": 2014,
+      "Total": 230
+   }
+}
+
+// assign the data 
+var datatotal = {
+
+	data2001: data2001,
+    data2002: data2002,
+    data2014: data2014
+};
